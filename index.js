1790963cd9d1fc41c2c015aa4658241c64aa5a7b
Marc Nicolau <mnicolau@kingeclient.com>
2021-05-08 23:05:57 +0200

    compilation test

--- index.js
+++ index.js
@@ -1,64 +1,16 @@
 const path = require('path');
 const { argv } = require('yargs');
 
-const generalConfig = {
-    entry: {
-        'main': path.resolve(__dirname, 'source/assets/scripts/main.js'),
-        'style': path.resolve(__dirname, 'source/assets/scss/style.scss'),
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
-        filename: 'js/[name].js'
-    },
-    scss: {
-        lint: {
-            path: path.resolve(__dirname, '/source/assets/scss/**/*.scss'),
-        }
-    },
-    img: {
-        from: path.join(__dirname, 'source/assets/img/'),
-        to: path.join(__dirname, 'dist/img')
-    },
-    browsersSync: [
-        {
-            host: 'localhost',
-            port: 3000,
-            server: {
-                baseDir: path.resolve(__dirname, 'dist')
-            },
-            proxy: 'http://localhost:3000/'
-        }, {
-            reload: false
-        }
-    ],
-}
-
-
-
-// module.exports = require('./webpack/webpack.config.js')(generalConfig);
-
-// const isNormal = !!((argv.type && argv.type == 'normal') || argv.normal);
-// const isTemplating = !!((argv.type && argv.type == 'templating') || argv.templating);
-
-// if (isNormal) {
-//     require('./webpack/webpack.config.js')(generalConfig);
-// }
-
-// if (isTemplating) {
-//     require('./webpack/templating.config.js')(generalConfig);
-// }
-
-module.exports = function(config = generalConfig , type = 'normal') {
+module.exports = function(config , type = 'normal') {
 
     const isNormal = !!((argv.type && argv.type == 'normal') || argv.normal || type == 'normal');
     const isTemplating = !!((argv.type && argv.type == 'templating') || argv.templating || type == 'templating');
 
     if (isNormal) {
-        require('./webpack/webpack.config.js')(config);
+        return require('./webpack/webpack.config.js')(config);
     }
     
     if (isTemplating) {
-        require('./webpack/templating.config.js')(config);
+        return require('./webpack/templating.config.js')(config);
     }
 }
